b06f841bec6344320c0229cf25dc21aac702572a
IvanSotomayor <ivanes99.ios@gmail.com>
2022-07-09 20:16:09 -0500

    fix some changes

--- src/main/java/com/google/sps/servlets/SentimenServlet.java
+++ src/main/java/com/google/sps/servlets/SentimenServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 
 @WebServlet("/sentiment")
-public class sentimentServlet extends HttpServlet {
+public class SentimentServlet extends HttpServlet {
     @Override
 public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String message = request.getParameter("message");
